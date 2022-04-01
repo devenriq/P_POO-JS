f71249e863dbf5016abce1f2714b32c7335997f1
Enrique <octepc.97@gmail.com>
2022-04-01 17:05:10 -0500

    actualizado los paths de las escuelas

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+class LearningPaths {
+  constructor({ name, numberClasses, timeLenght }) {
+    this.name = name;
+    this.listCourses = listCourses;
+    this.timeLenght = timeLenght;
+  }
+}
+
+const escuelaWeb = new LearningPaths({
+  name: "Escuela de Desarrollo Web",
+  listCourses: ["Curso definitivo de HTML", "Curso de CSS", "Curso de JS"],
+  timeLenght: 2345,
+});
+
+const escuelaDS = new LearningPaths({
+  name: "Escuela de Data Science",
+  listCourses: [
+    "Introducción a Python",
+    "Visualización de base de datos",
+    "Visualización de datos en Mongo",
+  ],
+  timeLenght: 1563,
+});
+
+const escuelaVGS = new LearningPaths({
+  name: "Escuela de Videojuegos",
+  listCourses: [
+    "Introducción a UnrealEngine",
+    "Introducción a C++",
+    "Modelado de entornos en Blender",
+  ],
+  timeLenght: 564,
+});
+
 class Student {
   constructor({
     name,
@@ -8,5 +42,30 @@ class Student {
     facebook = undefined,
     aprovedCourses = [],
     learningPaths = [],
-  }) {}
+  }) {
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.socialMedia = {
+      twitter,
+      instagram,
+      facebook,
+    };
+    this.aprovedCourses = aprovedCourses;
+    this.learningPaths = learningPaths;
+  }
 }
+
+const Enrique = new Student({
+  name: "Enrique",
+  username: "enrigios",
+  email: "octenrique@gmail.com",
+  twitter: "enrigios",
+});
+
+const Diana = new Student({
+  name: "Diana",
+  username: "dianasof",
+  email: "diansof@gmail.com",
+  instagram: "diansof",
+});
